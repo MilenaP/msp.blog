44855d8d445e3fe56df3be4d828914a4727bf606
milenap <milena_sky12@hotmail.com>
2014-03-01 14:26:52 -0500

    added prefix to links

--- config.rb
+++ config.rb
@@ -22,6 +22,7 @@ activate :blog do |blog|
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
+  blog.prefix="msp.blog"
 
   # Enable pagination
   # blog.paginate = true
@@ -108,7 +109,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
+  #activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
